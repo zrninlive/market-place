df929bb8e9ccda077ea9676fb0d6c5988fa07384
Christian Oliveira <christian.soliveira@outlook.com>
2019-07-13 18:27:07 -0300

    Create repository

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+
+class SessionController {
+  async store (req, res) {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+    if (!user) {
+      res.status(400).json({
+        error: 'User not found'
+      })
+    }
+
+    if (!await user.compareHash(password)) {
+      res.status(400).json({
+        error: 'Invalid password'
+      })
+    }
+
+    return res.json({ user, token: User.generateToken(user) })
+  }
+}
+
+module.exports = new SessionController()
